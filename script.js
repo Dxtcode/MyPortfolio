d333939c88dbc9cfad84ba8453fee5b5e19e99cb
Dexter Alvarado <dexteralvarado01@gmail.com>
2026-02-26 03:05:38 +0800

    Added blur background when hamburger menu is open

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
+  const overlay = document.querySelector('.overlay');
+
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+  overlay.classList.toggle('open');
+}
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+windows.onscroll = () => {
+    sections.forEach(sec => {
+      let stop = window.scrollY;
+      let offset = sec.offsetTop -  150;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
+
+    });
 }
